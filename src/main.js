a7c514430e36195ec36ad15b31b60aba1b5058f5
Cyrus <cyrus.frost@hotmail.com>
2019-04-29 21:23:22 +0530

    revert ed3e961518f81edcab1508c74d5e7fac52d996e3

--- src/main.js
+++ src/main.js
@@ -1,20 +1,17 @@
-import {
-  app,
-  BrowserWindow,
-  Tray,
-  Menu,
-  MenuItem,
-  NativeImage,
-  globalShortcut,
-  session
-} from 'electron';
-
+let app = require('electron').app;
+let AppMenu = require('electron').Menu;
+let MenuItem = require('electron').MenuItem;
+let AppTray = require('electron').Tray;
 let fileSystem = require('fs');
+let NativeImage = require('electron').nativeImage;
+let BrowserWindow = require('electron').BrowserWindow;
 let AutoLaunch = require("auto-launch");
 let log = require("electron-log");
 let join = require('path').join;
 let pjson = require('./package.json');
+let globalShortcut = require('electron').globalShortcut;
 let ContextMenu = require('electron-context-menu');
+let session = require('electron').session;
 
 app.requestSingleInstanceLock();
 
@@ -189,8 +186,6 @@ global.whatsApp = {
     global.whatsApp.iconStatus = 0;
     global.whatsApp.oldIconStatus = 0;
     global.whatsApp.newVersion = null;
-
-
     whatsApp.clearCache();
     whatsApp.openWindow();
     config.applyConfiguration();
@@ -198,9 +193,8 @@ global.whatsApp = {
 
   createMenu() {
     log.info("Creating menu");
-    whatsApp.menu =
-      Menu.buildFromTemplate(require('./AppMenu'));
-    Menu.setApplicationMenu(whatsApp.menu);
+    whatsApp.menu = AppMenu.buildFromTemplate(require('./AppMenu'));
+    AppMenu.setApplicationMenu(whatsApp.menu);
   },
 
   setNormalTray() {
@@ -258,10 +252,10 @@ global.whatsApp = {
     log.info("Creating tray icon");
     var trayImg = __dirname + '/assets/etc/trayTemplate.png';
     trayImg = __dirname + '/assets/icons/icon.png';
-    whatsApp.tray = new Tray(trayImg);
+    whatsApp.tray = new AppTray(trayImg);
 
     // Setting up a trayicon context menu
-    whatsApp.trayContextMenu = Menu.buildFromTemplate([{
+    whatsApp.trayContextMenu = AppMenu.buildFromTemplate([{
         label: ('Show'),
         visible: config.get("startminimized"), // Hide this option on start
         click: function () {
@@ -346,7 +340,6 @@ global.whatsApp = {
     whatsApp.window.on('closed', () => {
       whatsApp.window = null
     });
-
 
     ContextMenu({
       window: whatsApp.window
@@ -496,7 +489,6 @@ global.whatsApp = {
   }
 };
 
-
 global.settings = {
   init() {
     // if there is already one instance of the window created show that one
@@ -509,7 +501,7 @@ global.settings = {
   },
 
   createMenu() {
-    settings.menu = new Menu();
+    settings.menu = new AppMenu();
     settings.menu.append(new MenuItem({
       label: "close",
       visible: false,
@@ -627,7 +619,7 @@ global.phoneinfo = {
   },
 
   createMenu() {
-    phoneinfo.menu = new Menu();
+    phoneinfo.menu = new AppMenu();
     phoneinfo.menu.append(new MenuItem({
       label: "close",
       visible: false,
